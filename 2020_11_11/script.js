b574c3b35390d99d4e672491cd798e2eb7e7e982
20160450 <olympiodoros@naver.com>
2020-11-10 07:30:34 +0900

    initial commit

---
+++ 2020_11_11/script.js
@@ -0,0 +1,34 @@
+var formulaInput = document.getElementById("formula-input");
+var calcHistDiv = document.getElementById("calc-history");
+
+formulaInput.addEventListener("keyup", function (e) {
+    if (e.code === "Enter")
+        calculate();
+})
+
+function calculate() {
+
+    // 입력칸의 문자열이 사칙연산 형식이 맞는지 확인
+    var fm = formulaInput.value;
+    var formulaRegex = /^\d+(.\d+)?[+\-*/]{1}\d+(.\d+)?$/;
+    var formulaValid = formulaRegex.test(fm);
+
+    var resultText = "ERROR";
+    if (formulaValid) {
+        // 형식에 맞을 시 식을 계산하고 결과 문자열을 생성
+        var answer;
+        eval('answer=' + fm);
+        resultText = fm + " = ";
+        resultText += (answer % 1 > 0 ? answer.toFixed(2) : answer.toString());
+    }
+
+    // calc-history 상자에 넣을 또 다른 상자를 생성하고 내용을 설정한 뒤 삽입
+    var resultDiv = document.createElement("DIV");
+    resultDiv.appendChild(document.createTextNode(resultText));
+    if (!formulaValid)
+        calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+    calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+
+    // 입력칸은 빈칸으로
+    formulaInput.value = "";
+}
