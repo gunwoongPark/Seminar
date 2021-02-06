58bdc0755cad32616625a40666255d57f7ae15a8
20160450 <olympiodoros@naver.com>
2021-02-07 05:19:55 +0900

    no message

---
+++ 2021_02_09/vue_test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <div class="test">test</div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+.test {
+  background: aqua;
+}
+</style>
