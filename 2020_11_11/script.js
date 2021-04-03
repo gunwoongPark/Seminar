eb67f1550c7b60b36a11a1dcb3abf467f459f542
gunwoongPark <olympiodoros@naver.com>
2021-04-03 17:42:30 +0900

    no message

--- 2020_11_11/script.js
+++ 2020_11_11/script.js
@@ -2,25 +2,24 @@ var formulaInput = document.getElementById("formula-input");
 var calcHistDiv = document.getElementById("calc-history");
 
 formulaInput.addEventListener("keyup", function (e) {
-    if (e.code === "Enter")
-        calculate();
-})
+  if (e.code === "Enter") calculate();
+});
 
 function calculate() {
-    var fm = formulaInput.value;
-    var resultText = "ERROR";
-    var answer;
-    
-    eval('answer=' + fm);
-    resultText = fm + " = ";
-    resultText += (answer % 1 > 0 ? answer.toFixed(2) : answer.toString());
+  var fm = formulaInput.value;
+  var resultText = "ERROR";
+  var answer;
 
-    // calc-history 상자에 넣을 또 다른 상자를 생성하고 내용을 설정한 뒤 삽입
-    var resultDiv = document.createElement("DIV");
-    resultDiv.appendChild(document.createTextNode(resultText));
+  eval("answer=" + fm);
+  resultText = fm + " = ";
+  resultText += answer % 1 > 0 ? answer.toFixed(2) : answer.toString();
 
-    calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+  // calc-history 상자에 넣을 또 다른 상자를 생성하고 내용을 설정한 뒤 삽입
+  var resultDiv = document.createElement("DIV");
+  resultDiv.appendChild(document.createTextNode(resultText));
 
-    // 입력칸은 빈칸으로
-    formulaInput.value = "";
+  calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+
+  // 입력칸은 빈칸으로
+  formulaInput.value = "";
 }
